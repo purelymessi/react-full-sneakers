c44099143bbd86e8a3cd6eebd3eb0d72547b78d7
Cahad Rehimli"  git config --global user.name Your <cahadrehimli7@gmail.com>
2024-07-16 22:02:51 +0400

    new changes in project

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,41 +1,70 @@
-import "./Navbar.css"
+import "./Navbar.css";
 import logo from "../../assets/image/logo.png";
-import {LuShoppingCart , LuHeart , LuUserCircle} from  "react-icons/lu";
-
+import { LuShoppingCart, LuHeart, LuUserCircle } from "react-icons/lu";
+import { useState } from "react";
+import shoe from  "../../assets/image/shoe.png";
+import shoe1 from "../../assets/image/shoe1.png"
 
 function Navbar() {
+  const [cartOpen, setCartOpen] = useState(false);
+
+  const toggleCart = () => {
+    setCartOpen(!cartOpen);
+  };
+
   return (
     <header className="navbar">
-      
-    <nav className="navbar-nav container">
-    <div className="navbar-left">
-    <div className="navbar-logo">
-      <img src={logo} alt="logo" />
-    </div>
-    <div className="navbar-title">
-      <h1>REACT SNEAKERS</h1>
-      <p>Магазин лучших кроссовок</p>
-    </div>
-    </div>
-    <ul className="navbar-right">
-    
-      <li>
-        <LuShoppingCart size={18} color="#9B9B9B" />
-        <p>1205 руб.</p>
-      </li>
-      <li>
-        <LuHeart size={18} color="#9B9B9B"/>
-        <p>Закладки</p>
-      </li>
-      <li>
-        <LuUserCircle size={18} color="#9B9B9B" />
-        <p>Профиль</p>
-      </li>
-    
-    </ul>
+      <nav className="navbar-nav container">
+        <div className="navbar-left">
+          <div className="navbar-logo">
+            <img src={logo} alt="logo" />
+          </div>
+          <div className="navbar-title">
+            <h1>REACT SNEAKERS</h1>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="navbar-right">
+          <li onClick={toggleCart}>
+            <LuShoppingCart size={18} color="#9B9B9B" />
+            <p>1205 руб.</p>
+          </li>
+          <li>
+            <LuHeart size={18} color="#9B9B9B" />
+            <p>Закладки</p>
+          </li>
+          <li>
+            <LuUserCircle size={18} color="#9B9B9B" />
+            <p>Профиль</p>
+          </li>
+        </ul>
       </nav>
+      {cartOpen && (
+        <div className="cart-menu">
+          <h2>Корзина</h2>
+          <div className="cart-item">
+            <img src={shoe1} alt="" />
+            <div>
+              <p>Мужские Кроссовки Nike Air Max 270</p>
+              <h3>12 999 руб.</h3>
+            </div>
+          </div>
+          <div className="cart-item2">
+            <img src={shoe} alt="" />
+            <div>
+              <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
+              <h3>8 499 руб.</h3>
+            </div>
+          </div>
+          <div className="cart-summary">
+            <p>Итого: 21 498 руб.</p>
+            <p>Налог 5%: 1074 руб.</p>
+          </div>
+          <button className="summary-button">Оформить заказ</button>
+        </div>
+      )}
     </header>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,18 @@
 import "./ProductCard.css";
-import { LuPlus } from "react-icons/lu";
+import { useState } from "react";
+import { LuPlus , LuCheck , LuHeart} from "react-icons/lu";
 
 function ProductCard({ productTitle, productPrice, img }) {
+  const [selected, setSelected] = useState(false);
+  const [liked, setLiked] = useState(false);
   return (
     <div className="product-card">
+      <div className="product-like">
+      <button  className={liked && "liked"} onClick={() => setLiked(!liked)}>
+         <LuHeart color= {liked ? "#FF8585" : "#EAEAEA"} 
+         fill={liked ? "#FF8585" : "none"} size={16} />
+        </button>
+      </div>
       <div className="product-card__image">
         <img src={img} alt="Product" />
       </div>
@@ -13,12 +22,15 @@ function ProductCard({ productTitle, productPrice, img }) {
           <p>Цена:</p>
           <h5>{productPrice}</h5>
         </div>
-        <button>
-          <LuPlus size={11} color="#d3d3d3" />
+        <button 
+          className={selected && "selected" } 
+          onClick={() => setSelected(!selected)}
+        >
+          {!selected ? <LuPlus size={12} color="#d3d3d3" /> : <LuCheck />}
         </button>
       </div>
     </div>
   );
-}
+};
 
 export default ProductCard;
